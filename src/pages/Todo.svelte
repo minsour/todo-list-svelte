d44a7797beef70f15e27626d287ee5aae605d8b4
minsoopark <minsoopark@linecorp.com>
2020-09-24 18:32:25 +0900

    Add Todo page

---
+++ src/pages/Todo.svelte
@@ -0,0 +1,15 @@
+<script>
+  import TodoInput from "../components/TodoInput";
+  import TodoDetails from "../components/TodoDetails";
+  import TodoList from "../components/TodoList";
+</script>
+
+<div class="layout">
+  <div>
+    <TodoInput />
+    <TodoDetails />
+  </div>
+  <div>
+    <TodoList />
+  </div>
+</div>
