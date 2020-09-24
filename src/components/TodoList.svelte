df33ae8e6af26d0bebe6bbdd129548cd18adcfdb
minsoopark <minsoopark@linecorp.com>
2020-09-24 18:36:22 +0900

    Add TodoList component

---
+++ src/components/TodoList.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Todo from "./Todo.svelte";
+  import { todoList, filteredTodoList } from "../store/todoStore";
+  import TodoFilters from "./TodoFilters.svelte";
+</script>
+
+<div class="todoList">
+  {#if $todoList.length}
+    <TodoFilters />
+  {/if}
+  <ul class="listContainer">
+    {#each $filteredTodoList as todo (todo.id)}
+      <Todo {todo} />
+    {/each}
+  </ul>
+</div>
